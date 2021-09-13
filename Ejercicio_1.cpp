b9b6892ff1585d0ec058ccbc8ba9c25cdadc99c7
Guillermo Arguello Cordobe <74156373+Elmemoing@users.noreply.github.com>
2021-09-13 09:26:13 -0400

    Create Ejercicio_1.cpp
    Imprimir los números hasta 100 de forma ascendente

---
+++ Ejercicio_1.cpp
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stlib.h>
+
+#define MAX 100
+
+int main()
+{
+  int x, numero[MAX];
+  
+  for (x=0; x<=numero[MAX]; x++)
+      {
+        numero[MAX]=x;
+      }
+  for (x=0; x<=numero[MAX]; x++)
+      {
+        printf("%d",numero[MAX]);
+      }
+  return 0;
+}  
