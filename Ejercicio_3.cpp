0d54a37a5a9191d15e3be04efd1ca88c9555bc57
Guillermo Arguello Cordobe <74156373+Elmemoing@users.noreply.github.com>
2021-09-14 19:04:00 -0400

    Update Ejercicio_3.cpp

--- Ejercicio_3.cpp
+++ Ejercicio_3.cpp
@@ -9,28 +9,28 @@
 
 int main()
 {
-	int x,cont,x2,cont2,numero[MAX];
-	cont2=0;
+   int x,x2,cont,cont2,numero[MAX];
+   cont2=0;
 	
-	for(x=1; x<=MAX; x++)
-		{
-			cont=0;
-			for (x2=1; x2<=x; x2++)
-				{
-					if (x%x2 == 0)
-						{
-			   				cont++;
-		    			}
-		    	}
-			if (cont==2 || x2==1 || x2==0)
-				{
-					numero[cont2] = x;
-					cont2++;
-				}
-		}
-	for(x=1; x<cont2; x++)
-		{
-			printf("%d\n", numero[x]);
-		}
-	return 0;
+   for(x=1; x<=MAX; x++)  /*Recorre las posiciones del arreglo*/
+      {
+	cont=0;
+	for (x2=1; x2<=x; x2++)
+	     {
+	       if (x%x2 == 0)  /*Si el numero es divisor entonces no es primo*/	
+	       	   {
+	             cont++;
+		   }
+	     }
+         if (cont==2 || x2==1 || x2==0) /*Las tres primeras posiciones del arreglo*/
+             {
+             	numero[cont2] = x;
+	   	cont2++;
+             }
+      }
+      for(x=1; x<cont2; x++)
+	  {
+         	printf("%d\n", numero[x]);
+	  }
+  return 0;
 }
