5f40c7cac7eb9b78d0e4ed8210e3820cbf97be88
Guillermo Arguello Cordobe <74156373+Elmemoing@users.noreply.github.com>
2021-10-04 22:11:45 -0300

    Ejercicio 12

--- Ejercicio12.cpp
+++ Ejercicio12.cpp
@@ -1,4 +1,4 @@
-/*12. Que lea 10 números por teclado, los almacene en un array y muestre el promedio.*/
+/*Ejercicio 12. Que lea 10 números por teclado, los almacene en un array y muestre el promedio.*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,18 +9,15 @@ int main()
   int i;
 
   sum=0;
-  for (i=0;i<10;i++){
-                    printf("Escriba un número");
-                    scanf("%f",&numeros1[i]);
+  for (i=0;i<10;i++)
+       {
+         printf("Escriba un número");
+         scanf("%f",&numeros1[i]);
       }
-     
   for(i=0;i<10;i++)
-  {
-   sum=sum+numeros1[i];
-  }
- 
-  printf("%f\n",sum/10);
-     
-  system("PAUSE");     
+      {
+        sum=sum+numeros1[i];
+      } 
+  printf("%f\n",sum/10);     
   return 0;
 }
